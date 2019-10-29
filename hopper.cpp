0df8686ab1f7e26cc4acaebabdb54ef0b6a8f6cf
Marissa Liu <marissajliu@gmail.com>
2019-10-28 21:34:21 -0400

    more typos lol

--- hopper.cpp
+++ hopper.cpp
@@ -7,18 +7,18 @@ Hopper::getHopperID() {
   return this->hopper_ID;
 }
 
-Hopper::getCandyType() {
+string Hopper::getCandyType() {
   return this->candy_type;
 }
 
-Hopper::setCandyType(string new_type) {
+void Hopper::setCandyType(string new_type) {
 }
 
-Hopper::getLastRefillTime() {
+int Hopper::getLastRefillTime() {
 
 
 }
 
-Hopper::setLastRefillTime(int new_time) {
+void Hopper::setLastRefillTime(int new_time) {
 
 }
