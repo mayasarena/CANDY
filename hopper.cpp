5529a4c4b091551ae166162c486545396599003d
Fletcher Laptop <Fletcher Laptop@LAPTOP-MMM09C6R>
2019-10-28 21:25:31 -0400

    hopper plzwrk

--- hopper.cpp
+++ hopper.cpp
@@ -1,1 +1,29 @@
 // Hello word!
+// Hello word!
+#include <string.h>
+
+
+int hopperID;
+string candyType;
+int lastRefillTime;
+
+int getHopperID(){
+return hopperID;		
+}
+
+string getCandyType(){
+return candyType;
+}
+
+void setCandyType(string input){
+}
+
+int getLastRefillTime(){
+
+
+}
+
+void setLastRefillTime(){
+
+
+}
